c4dcb2ed44c327a13c5340f17793a4940c9205b7
Harris <harriskarume.hk@gmail.com>
2022-11-09 08:31:37 +0300

    Implement sql connection

---
+++ app.ts
@@ -0,0 +1,14 @@
+import mysql from 'mysql'
+import * as  dotenv from 'dotenv'
+dotenv.config()
+
+let con = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASS
+})
+
+con.connect((err: any) => {
+    if (err) throw err
+    console.log('connected')    
+})
