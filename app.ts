08c3cd8d46770d7028e6a4bcf9f4797b0d58839c
Harris <harriskarume.hk@gmail.com>
2022-11-28 23:37:44 +0300

    make use of createPool

--- app.ts
+++ app.ts
@@ -4,6 +4,7 @@ dotenv.config()
 
 let con = mysql.createConnection({
     host: process.env.HOST,
+    port: process.env.PORT as unknown as number,
     user: process.env.USER,
     password: process.env.PASS
 })
@@ -12,3 +13,27 @@ con.connect((err: any) => {
     if (err) throw err
     console.log('Connected succefully')    
 })
+
+// let pool = mysql.createPool({
+//     connectionLimit: 10,
+//     host: process.env.HOST,
+//     user: process.env.USER,
+//     password: process.env.PASS,
+//     database: process.env.DB,
+//     port: process.env.PORT as unknown as number
+// })
+
+// pool.getConnection((err, connection) => {
+//     if(err) throw err
+
+//     if (connection) {
+//         console.log('Connected succesfully');
+//         connection.release()
+//         return
+//     }
+// })
+
+// pool.query('SELECT 1 + 1 AS solution', function (error, results) {
+//     if (error) throw error;
+//     console.log('The solution is: ', results[0].solution)
+// })
